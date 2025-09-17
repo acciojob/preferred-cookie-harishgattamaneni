920baeb6025e16b8bc6dca6905704d82d5c86726
harishgattamaneni <103264547+harishgattamaneni@users.noreply.github.com>
2025-09-17 09:46:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+function saveCookie(event){
+    event.preventDefault()
+    let fontSize=document.getElementById("fontsize").value
+    let fontColor = document.getElementById("fontcolor").value
+    console.log(fontSize)
+    console.log(fontColor)
+    document.cookie = `fontsize=${fontSize};`
+     document.cookie = `fontcolor=${fontColor};`
+}
