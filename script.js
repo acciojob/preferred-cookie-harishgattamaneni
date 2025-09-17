095a27539db53a792a2c69c31aea2e7d6904ea4b
harishgattamaneni <103264547+harishgattamaneni@users.noreply.github.com>
2025-09-17 10:11:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ function saveCookie(event){
     document.cookie = `fontsize=${fontSize};`
      document.cookie = `fontcolor=${fontColor};`
 }
+
+function getCookie(name){
+    let cookies= document.cookie.split("; ")
+    for( let c of cookies){
+        let [key, value]=c.split("=");
+        if(key === name){
+            return value;
+        }
+    }
+    return null;
+}
+
+window.onload = function(){
+    let fontSize= getCookie("fontsize")
+    let fontColor=getCookie("fontcolor")
+    if (!isNaN(fontSize)) {
+        fontSize = fontSize + "px";
+      }
+    if(fontSize){
+        document.body.style.fontSize= fontSize;
+    }
+    if(fontColor){
+        document.body.style.color= fontColor;
+    }
+}
